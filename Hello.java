f619e1ff540d95695734c3537966f0eadd272715
6210530017 <60337747+6210530017@users.noreply.github.com>
2020-01-27 14:57:03 +0700

    Add Hello1.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,7 @@
 public class Hello{
   public static void main(String[] args){
     System.out.println("Hi");
+    System.out.println(1/1+1/2+1/3+1/4+1/5);
+    System.out.println(1/1.0+1/2.0+1/3.0+1/4.0+1/5.0);
   } 
 }
